abc4335b60dc926b12a4a73b522a9133ad5c2dde
Conner Gleason <connereg@optonline.net>
2022-07-27 16:10:10 -0400

    Fully Completed Lab

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,56 +1,55 @@
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Random;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
 
 public class Main {
     public static void main(String[] args) throws Exception {
-        ExecutorService executor = Executors.newFixedThreadPool(9);
-        List<Future> listOfStringFutures = new ArrayList<>();
+        ExecutorService executor = Executors.newSingleThreadExecutor();
+        List<Future> listOfFutures = new ArrayList<>();
 
 
-        listOfStringFutures.add(executor.submit(() -> " The Future is extremely bright!"));
-        listOfStringFutures.add(executor.submit(() -> " The Future is now old man!"));
-        listOfStringFutures.add(executor.submit(() -> " The Future will one day be present."));
-        listOfStringFutures.add(executor.submit(() -> " The Future is extremely bright!"));
-        listOfStringFutures.add(executor.submit(() -> " The Future is now old man!"));
-        listOfStringFutures.add(executor.submit(() -> " The Future will one day be present."));
+    for (int i = 0; i < 7; i++) {
+        Future<Integer> futureInt = executor.submit(() -> {
+            Random randInt = new Random();
+            int num = randInt.nextInt(100);
+            return num;
+        });
+        // INSIDE LOOP, BUT OUTSIDE INSTANCIATION OF futureInt
+        listOfFutures.add(futureInt);
+    }
 
-        Future<String> futureString4 = executor.submit(() -> (" The Future is extremely DARK!"));
-        Future<String> futureString5 = executor.submit(() -> " The Future is now young man!");
-        Future<String> futureString6 = executor.submit(() -> " The Future will one day be past.");
 
+        listOfFutures.get(4).cancel(true);
+        listOfFutures.get(5).cancel(true);
+        listOfFutures.get(3).cancel(true);
 
-        listOfStringFutures.add(futureString4);
-        listOfStringFutures.get(4).cancel(true);
-        listOfStringFutures.add(futureString5);
-        listOfStringFutures.get(5).cancel(true);
-        listOfStringFutures.add(futureString6);
-        listOfStringFutures.get(6).cancel(true);
+        for(Future f: listOfFutures){
+            if(!f.isCancelled()){
+                f.get();
+            }
+        }
 
-        System.out.println("Count finished before thread sleep: " + countFinishedFutures(listOfStringFutures));
+        System.out.println("Count finished before thread sleep: " + countFinishedFutures(listOfFutures));
         Thread.sleep(200);
-        System.out.println("Count finished AFTER thread sleep: " + countFinishedFutures(listOfStringFutures));
-
-
-//        System.out.println("Now we can call for future values: " + futureString1.get() + futureString2.get() + futureString3.get());
+        System.out.println("Count finished AFTER thread sleep: " + countFinishedFutures(listOfFutures));
 
     }
 
     public static int countFinishedFutures(List<Future> futures) {
-        // your code here
-        int futureNum = 0;
-        for (Future future : futures) {
-            if (!future.isCancelled()) {
-                if (future.isDone()) {
-                    futureNum++;
-                }
-            }
-            else {
-                System.out.println("A Future is not eligable");
-            }
-        }
-        return futureNum;
+    // ROTE CODE (NON-STREAM) COMMENTED OUT
+//        int futureNum = 0;
+        return (int)futures.stream().filter(future -> !future.isCancelled()).filter(future -> future.isDone()).count();
+
+//        for (Future future : futures) {
+//            if (!future.isCancelled()) {
+//                if (future.isDone()) {
+//                    futureNum++;
+//                }
+//            }
+//        }
+//        return futureNum;
     }
 }
